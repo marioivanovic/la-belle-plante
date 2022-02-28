df74c653902b7d21b7ee6e49393af00152e20b2a
mrmarioi <ivanovic.mario@gmail.com>
2022-02-28 15:58:18 +0100

    :beers: Product page modification :beers:

--- la_belle_plante/src/app/pages/page-product/page-product.component.ts
+++ la_belle_plante/src/app/pages/page-product/page-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PlantService } from 'src/app/services/plant.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-page-product',
@@ -8,8 +11,30 @@ import { Component, OnInit } from '@angular/core';
 export class PageProductComponent implements OnInit {
   private data: any[] | undefined;
   public listCategories!: string[];
+  private subListProduct: Subscription;
   public listProduct!: any[];
-  constructor() {}
+
+  constructor(private plantService: PlantService) {
+    this.subListProduct = this.plantService.subjectListProduct$.subscribe(
+      (response) => {
+        console.log(response);
+        this.data = response;
+        this.listCategories = _.uniq(
+          this.data.map((x) => x.product_breadcrumb_label)
+        );
+        console.log(this.listCategories);
+
+        response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
+        this.listProduct = [...response];
+      }
+    );
+
+    this.plantService.getListProductsChaud();
+  }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subListProduct.unsubscribe();
+  }
 }
