65eb95485a930b6bbab3ae34f90cddba2ab15581
mrmarioi <ivanovic.mario@gmail.com>
2022-03-01 13:05:27 +0100

    :construction:

--- la_belle_plante/src/app/pages/page-product/page-product.component.ts
+++ la_belle_plante/src/app/pages/page-product/page-product.component.ts
@@ -10,32 +10,30 @@ import * as _ from 'underscore';
   styleUrls: ['./page-product.component.scss'],
 })
 export class PageProductComponent implements OnInit {
-  @Input() plant: any;
-  private data: any[] | undefined;
-  private subListProduct: Subscription;
-  public listProduct!: any[];
+  // private data: any[] | undefined;
+  // private subListProduct: Subscription;
+  // public listProduct!: any[];
+  public plant: any;
 
   constructor(
     private plantService: PlantService,
     private active: ActivatedRoute
   ) {
-    this.subListProduct = this.plantService.subjectListProduct$.subscribe(
-      (response) => {
-        console.log('hop', response);
-        this.data = response;
-        this.listProduct = _.uniq(this.data.map((x) => x.product_id));
-        console.log('coucou', this.listProduct);
-
-        response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
-        this.listProduct = [...response];
-      }
-    );
-
-    this.plantService.getListProductsChaud();
+    // this.subListProduct = this.plantService.subjectListProduct$.subscribe(
+    //   (response) => {
+    //     console.log('hop', response);
+    //     this.data = response;
+    //     this.listProduct = _.uniq(this.data.map((x) => x));
+    //     console.log('coucou', this.listProduct);
+    //     response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
+    //     this.listProduct = [...response];
+    //   }
+    // );
+    // this.plantService.getListProductsChaud();
   }
 
   ngOnInit(): void {
-    const id = this.active.snapshot.queryParamMap.get('productId');
+    const id = this.active.snapshot.queryParamMap.get('id');
 
     if (id) {
       this.plantService.getById(id).subscribe((product) => {
@@ -46,7 +44,5 @@ export class PageProductComponent implements OnInit {
   }
 
   // methode de cycle de vie de mon composant qui est executée juste avant que l'instance de mon composant soit détruite
-  ngOnDestroy(): void {
-    this.subListProduct.unsubscribe();
-  }
+  ngOnDestroy(): void {}
 }
