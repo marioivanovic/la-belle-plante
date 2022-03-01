de86ef3b6d7a2e368d921a367de3d31595601575
mrmarioi <ivanovic.mario@gmail.com>
2022-03-01 15:59:05 +0100

    :warning: important changes :warning:

---
+++ src/app/pages/page-product/page-product.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { PlantService } from 'src/app/services/plant.service';
+import * as _ from 'underscore';
+
+@Component({
+  selector: 'app-page-product',
+  templateUrl: './page-product.component.html',
+  styleUrls: ['./page-product.component.scss']
+})
+export class PageProductComponent implements OnInit {
+  // private data: any[] | undefined;
+  // private subListProduct: Subscription;
+  // public listProduct!: any[];
+  public plant: any;
+
+  constructor(
+    private plantService: PlantService,
+    private active: ActivatedRoute
+  ) {
+    // this.subListProduct = this.plantService.subjectListProduct$.subscribe(
+    //   (response) => {
+    //     console.log('hop', response);
+    //     this.data = response;
+    //     this.listProduct = _.uniq(this.data.map((x) => x));
+    //     console.log('coucou', this.listProduct);
+    //     response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
+    //     this.listProduct = [...response];
+    //   }
+    // );
+    // this.plantService.getListProductsChaud();
+  }
+
+  ngOnInit(): void {
+    const id = this.active.snapshot.queryParamMap.get('id');
+
+    if (id) {
+      this.plantService.getById(id).subscribe(product => {
+        console.log('detail ----> ', product);
+        this.plant = product[0];
+      });
+    }
+  }
+
+  // methode de cycle de vie de mon composant qui est executée juste avant que l'instance de mon composant soit détruite
+  ngOnDestroy(): void {}
+}
