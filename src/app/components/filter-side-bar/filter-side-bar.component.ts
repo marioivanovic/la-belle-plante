56a333e57c293f045d1dd3dcd033601f533a1689
Max Surnin <madmax8930@gmail.com>
2022-03-03 11:44:37 +0100

    test done 3 fonctionnalities done

--- src/app/components/filter-side-bar/filter-side-bar.component.ts
+++ src/app/components/filter-side-bar/filter-side-bar.component.ts
@@ -1,21 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { PlantService } from 'src/app/services/plant.service';
+
 
 @Component({
   selector: 'app-filter-side-bar',
   templateUrl: './filter-side-bar.component.html',
   styleUrls: ['./filter-side-bar.component.scss']
 })
+
 export class FilterSideBarComponent implements OnInit {
+  selectedCategory : string[];
   @Input() listCategoriesFilter: string[];
+  @Output() filterArray: EventEmitter<any> = new EventEmitter;
 
-  constructor() {
+  constructor(private plantService: PlantService) {
     this.listCategoriesFilter = [];
-    console.log(' =>', this.listCategoriesFilter);
+    this.selectedCategory = [];
+    // this.plantService.myCategorys(this.selectedCategory)
+   }
+
+  ngOnInit(): void {
   }
 
-  ngOnInit(): void {}
+  public onChangeValue(eventValue: any , category : string): void {
+    if (eventValue.target.checked ) {
 
-  public onChangeValue(eventValue: any): void {
-    console.log('Event Change :', eventValue);
+      // console.log(category + " checked");
+       this.selectedCategory.push(category)
+    } else {
+
+      // console.log(category + " unchecked");
+       this.selectedCategory = this.selectedCategory.filter(c=>c!=category);
+    }
+    // console.log(this.selectedCategory);
+     this.filterArray.emit(this.selectedCategory);
   }
+
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrlApi: 'http://localhost:3000'
+  baseUrlApi: 'https://la-belle-plante-test.herokuapp.com'
 };
 
 /*
