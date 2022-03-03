5ea3bba980b0dea6197ba67755aad4b6aebbb73a
Mario Ivanovic <62057742+marioivanovic@users.noreply.github.com>
2022-03-03 14:09:18 +0100

    Update page-product.component.ts

--- src/app/pages/page-product/page-product.component.ts
+++ src/app/pages/page-product/page-product.component.ts
@@ -10,28 +10,16 @@ import * as _ from 'underscore';
 })
 export class PageProductComponent implements OnInit {
   private data: any[] | undefined;
-  // private subListProduct: Subscription;
-  // public listProduct!: any[];
   public plant: any;
 
   constructor(
     private plantService: PlantService,
     private active: ActivatedRoute
   ) {
-    // this.subListProduct = this.plantService.subjectListProduct$.subscribe(
-    //   (response) => {
-    //     this.data = response;
-    //     this.listProduct = _.uniq(this.data.map((x) => x));
-    //     response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
-    //     this.listProduct = [...response];
-    //   }
-    // );
-    // this.plantService.getListProductsChaud();
     const id = this.active.snapshot.queryParamMap.get('id');
 
     if (id) {
       this.plantService.getById(id).subscribe(product => {
-        console.log('detail ----> ', product);
         this.data = product;
         this.plant = product;
       });
