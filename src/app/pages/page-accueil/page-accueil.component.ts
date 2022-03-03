2bd27411b2800863de5e976b6f8e89df6b423cfe
mrmarioi <ivanovic.mario@gmail.com>
2022-03-03 15:12:30 +0100

    test 1

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { PlantService } from 'src/app/services/plant.service';
 import * as _ from 'underscore';
-import { Options , LabelType } from "@angular-slider/ngx-slider";
+import { Options, LabelType } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-page-accueil',
@@ -11,11 +11,11 @@ import { Options , LabelType } from "@angular-slider/ngx-slider";
 })
 export class PageAccueilComponent implements OnInit {
   // private data!: any[]; same as below
-  private data: any[] | undefined;  /* Declare le type de ma propriété */
+  private data: any[] | undefined; /* Declare le type de ma propriété */
   public listCategories!: string[]; /* ! : type of prop could be undefined */
   private subListProduct: Subscription;
   public listProduct!: any[];
-  public term!: "";
+  public term!: '';
   public listProductFiltered!: any[];
   public isAscendingSort: boolean = false;
   public tab: any = [];
@@ -25,21 +25,19 @@ export class PageAccueilComponent implements OnInit {
   options: Options = {
     floor: 0,
     ceil: 150,
-    translate: (value: number, label: LabelType): string => {  
-        switch (label) {  
-            case LabelType.Low:  
-                return "<b>Min price:</b> $" + value;  
-            case LabelType.High:  
-                return "<b>Max price:</b> $" + value;  
-            default:  
-                return "$" + value;  
-        }  
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return value + '<b> Mini</b> €';
+        case LabelType.High:
+          return value + '<b> Max</b> €';
+        default:
+          return value + '€';
+      }
     }
-  }
-
+  };
 
   constructor(private plantService: PlantService) {
-    
     this.subListProduct = this.plantService.subjectListProduct$.subscribe(
       response => {
         this.data = response;
@@ -51,22 +49,23 @@ export class PageAccueilComponent implements OnInit {
       }
     );
 
-    this.subListProduct = this.plantService.subjectListProduct$.subscribe(response => {
-     console.log(response);
-     this.data = response;
-     this.listCategories = _.uniq(this.data.map (x => x.breadcrumb_label));
-     console.log(this.listCategories);
+    this.subListProduct = this.plantService.subjectListProduct$.subscribe(
+      response => {
+        console.log(response);
+        this.data = response;
+        this.listCategories = _.uniq(this.data.map(x => x.breadcrumb_label));
+        console.log(this.listCategories);
 
-    //  response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
-     this.listProduct = [...response];
-     this.listProductFiltered = this.listProduct
-   })
-   this.plantService.getListProductsChaud();
-   // declancher la req API et la resp est transmise avec un Subject
+        //  response.length = 40; // juste pour le dev dans notre contexte d'apprentissage
+        this.listProduct = [...response];
+        this.listProductFiltered = this.listProduct;
+      }
+    );
+    this.plantService.getListProductsChaud();
+    // declancher la req API et la resp est transmise avec un Subject
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   // Methode de cycle de vie de mon composant qui est executee
   // juste avant l'instance de mon composant soit detruite
 
@@ -75,36 +74,37 @@ export class PageAccueilComponent implements OnInit {
   }
 
   addItem(term: any) {
-
     this.plantService.subjectListProduct$.subscribe(products => {
       if (term.trim() != '') {
-      this.listProduct = products.filter(product => {
-          return (product.name.toLowerCase().indexOf(term.toLowerCase()) > -1)
-      })
-    } else {
-      this.listProduct = products;
-    }
-  })
+        this.listProduct = products.filter(product => {
+          return product.name.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        });
+      } else {
+        this.listProduct = products;
+      }
+    });
     this.plantService.getListProductsChaud();
   }
 
   displayItem($event: any) {
-
-      this.plantService.subjectListProduct$.subscribe(listProductFiltered => {
-        this.listProductFiltered = listProductFiltered.filter(product => {
-          return product.unitprice_ati >= $event.value && product.unitprice_ati <= $event.highValue
-        });
-        // console.log($event.value)
-        // console.log($event.highValue)
-        console.log(listProductFiltered)
-        this.tab = [$event.value, $event.highValue]
-        console.log(this.tab)
-      })  
-      this.plantService.getListProductsChaud();
-  };
+    this.plantService.subjectListProduct$.subscribe(listProductFiltered => {
+      this.listProductFiltered = listProductFiltered.filter(product => {
+        return (
+          product.unitprice_ati >= $event.value &&
+          product.unitprice_ati <= $event.highValue
+        );
+      });
+      // console.log($event.value)
+      // console.log($event.highValue)
+      console.log(listProductFiltered);
+      this.tab = [$event.value, $event.highValue];
+      console.log(this.tab);
+    });
+    this.plantService.getListProductsChaud();
+  }
 
   onClickBtn(arr: any): void {
-    console.log(`Event Change : $eventValue`)
+    console.log(`Event Change : $eventValue`);
     this.isAscendingSort != this.isAscendingSort;
     arr.sort((a: any, b: any) => {
       if (a.unitprice_ati < b.unitprice_ati) {
@@ -118,27 +118,18 @@ export class PageAccueilComponent implements OnInit {
     return arr;
   }
 
-  changeCategory(event:any) {
-
-    console.log("arrayCat",event);
+  changeCategory(event: any) {
+    console.log('arrayCat', event);
     if (event.length !== 0) {
-      this.listProductFiltered = this.listProduct.filter(x => event.includes(x.breadcrumb_label));
-      this.listProductFiltered = this.listProductFiltered.filter(x => x.unitprice_ati >= this.tab[0] && x.unitprice_ati <= this.tab[1]);
+      this.listProductFiltered = this.listProduct.filter(x =>
+        event.includes(x.breadcrumb_label)
+      );
+      this.listProductFiltered = this.listProductFiltered.filter(
+        x => x.unitprice_ati >= this.tab[0] && x.unitprice_ati <= this.tab[1]
+      );
     } else {
       this.listProductFiltered = this.listProduct;
       window.location.reload();
     }
-  displayItem($event: any) {
-    this.plantService.subjectListProduct$.subscribe(listProduct => {
-      this.listProduct = listProduct.filter(product => {
-        return (
-          product.unitprice_ati >= $event.value &&
-          product.unitprice_ati <= $event.highValue
-        );
-      });
-    });
-    this.plantService.getListProductsChaud();
   }
-
 }
-  
